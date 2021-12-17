90854379821150782ee694650f3654d7739c68d5
mrcs22 <marcusmoraes2010@hotmail.com>
2021-12-17 11:10:09 -0300

    Preparando revisão

--- src/componentes/ButtonFrontFace/index.js
+++
@@ -1,9 +0,0 @@
-import Turn from "../../assets/turn.png"
-
-export default function ButtonFrontFace({setquestao}){
-    return(
-        <button className="virar" data-identifier="arrow" onClick={() => setquestao(false)}>
-            <img src={Turn}></img>
-        </button>
-    );
-}
